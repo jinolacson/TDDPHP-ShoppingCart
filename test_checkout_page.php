66aaf743816d39ee7d7b289234708c6a99d75c80
jino <jlacson@wylog.com>
2018-10-17 17:04:48 +0800

    Added 1 failed test for CheckoutStubTest.php
    Line 83:testItemsDetailsIfNotArrayOrNull
    
    expected result should be an empty cart

--- test_checkout_page.php
+++ test_checkout_page.php
@@ -1,16 +1,17 @@
 <?php
 require __DIR__ .'/vendor/autoload.php';
 
+//load classes
 use Classes\Products;
 use Classes\Carts;
 use Classes\Checkouts;
 
-
 session_start();
 
+//inject dependecies
 $chk = new Checkouts(new Carts(new Products()));
 
-
+//print checkout details including discouted price
 echo "<pre>";
 print_r($chk->getDiscount()); echo "<br>";echo "<br>";
 print_r($chk->checkoutPriceDetails()); echo "<br>";
--- tests/CheckoutStubTest.php
+++ tests/CheckoutStubTest.php
@@ -6,9 +6,24 @@ use Classes\Checkouts;
 
 use PHPUnit\Framework\TestCase;
 
+interface MyThrowable extends Throwable
+{
+    public function trigger_exception(string $error);
+}
+
+class MyException extends Exception implements MyThrowable
+{
+    public function trigger_exception(string $error)
+    {
+        throw new Exception('Render error.'.$error);
+    }
+}
+
 /**
  * CheckoutStubTest
  * @group group
+ * 
+ * @requires PHP 7.2
  */
 class CheckoutStubTest extends TestCase
 {
@@ -22,35 +37,68 @@ class CheckoutStubTest extends TestCase
 
     public function setUp()
     {
+        //instantiate mock builder
         $this->chk = $this->createMock(Checkouts::class);
     }
     
+    /**
+     * Test for total discounted items
+     *
+     * @return void
+     */
     public function testDiscountedPriceShouldReturnAnInteger()
     {
         $this->chk->method('validItemsDiscount')->willReturn(250);
-        $this->assertEquals(250, $this->chk->validItemsDiscount(),"Discount should be: 250");
+        $this->assertEquals(250, $this->chk->validItemsDiscount(), "Discount should be: 250");
     }
 
+    /**
+     * Test for total original price
+     *
+     * @return void
+     */
     public function testOriginalPriceShouldReturnAnInteger()
     {
         $this->chk->method('originalPrice')->willReturn(1000);
-        $this->assertSame(1000, $this->chk->originalPrice(),"Original price should be: 1000");
+        $this->assertSame(1000, $this->chk->originalPrice(), "Original price should be: 1000");
     }
     
-    
+    /**
+     * Test for Original price stripped comma
+     *
+     * @return void
+     */
     public function testPriceShouldStripComma()
     {
         $this->chk->method('removePriceComma')->willReturn(5000);
-        $this->assertEquals(5000, $this->chk->removePriceComma(1,000),"Striped comma price should be: 5000");
+        $this->assertEquals(5000, $this->chk->removePriceComma(1, 000), "Striped comma price should be: 5000");
     }
 
-
+    /**
+     * Test checkout items detail
+     * Assume a failed test, should be an empty Item details first
+     *
+     * @return void
+     */
     public function testItemsDetailsIfNotArrayOrNull()
     {
-        $this->chk->method('checkoutItemsDetails')->will($this->returnValue(null)); //[ "AVR" => 2000 ]
-        $this->assertTrue(!is_array($this->chk->checkoutItemsDetails()),"Items should be in an empty array!.");
+        $this->chk->method('checkoutItemsDetails')->will($this->returnValue([ "AVR" => 2000 ]));
+        $this->assertTrue(!is_array($this->chk->checkoutItemsDetails()), "Items should be empty cart!.");
     }
     
+    /**
+     * @expectedException MyException
+     */
+    // public function testImplementationForErrorHandler()
+    // {
+    //     $ex = new MyException();
+    //     $ex->trigger_exception("Skipped test");
+
+    //     $this->markTestSkipped(
+    //         'Mark incomplete'
+    //       );
+    // }
+
     public function tearDown()
     {
         parent::tearDown();
